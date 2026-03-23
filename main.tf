8bdcd9c526648ccf1c91b332b87d1211ff71bfd8
LeoPincle <leo.soto965@gmail.com>
2026-03-23 13:11:04 -0600

    feat: provision EKS cluster with managed node group using Terraform module

--- main.tf
+++ main.tf
@@ -12,3 +12,24 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 }
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 1
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
