fdf4d1daea52946fe60072bf221693c9b2a9ba06
LeoPincle <leo.soto965@gmail.com>
2026-03-23 15:07:48 -0600

    feat: enable public access to EKS cluster endpoint for external connectivity

--- main.tf
+++ main.tf
@@ -34,6 +34,9 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
 
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true
+
   eks_managed_node_groups = {
     default = {
       desired_size = 1
