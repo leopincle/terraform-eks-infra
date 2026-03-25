2ba9d52af8c356b44969ad5eb7403a09e6e97239
LeoPincle <leo.soto965@gmail.com>
2026-03-25 14:31:38 -0600

    chore(ci): add terraform cache cleanup step per environment (infra/platform)

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,6 +6,6 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "cluster_ca_certificate" {
+output "cluster_ca_certificate_authority_data" {
   value = module.eks.cluster_certificate_authority_data
 }
